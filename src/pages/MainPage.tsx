fdc6164b8487752bc21f55b1ec74d56581397e47
Yves <withcenter.dev12@gmail.com>
2025-08-29 16:56:37 +0800

    .

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -32,6 +32,27 @@ export default function MainPage() {
 
   return (
     <>
+      {/* Page specific SEO – React Document Metadata v19 */}
+      <title>링구</title>
+      <meta name="description" content="링크로 시작하는 간편 구독 서비스" />
+      <meta
+        name="keywords"
+        content="링구, 간편구독, 구독서비스, 누구나 쉽고 빠르게 구독, 구독, 소상공인, 학원비, 렌터카, 후원금"
+      />
+      <meta name="author" content="링구" />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="링구" />
+      <meta
+        property="og:description"
+        content="링크로 시작하는 간편 구독 서비스"
+      />
+      <meta property="og:url" content="" />
+      <meta
+        property="og:image"
+        content="https://xn--2e0bw7u.com/img/ogimg.png"
+      />
+
       <Header onOpen={toggleOverlay} />
       <main className="mx-auto flex flex-col items-center overflow-x-hidden sm:w-[1200px]">
         <HeroSection />
